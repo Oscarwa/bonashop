5d833f01ec50061fe4981240c4615e8b9ec8d1ee
Oscarwa <racso_181088@hotmail.com>
2018-09-24 11:55:58 -0500

    Changed title on product detail

--- shop/src/app/detail/detail.component.ts
+++ shop/src/app/detail/detail.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Product } from '../product';
 import { ProductService } from '../product.service';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Title } from '@angular/platform-browser'
 
 @Component({
   selector: 'app-detail',
@@ -16,24 +17,31 @@ export class DetailComponent implements OnInit {
   hasDiscount: boolean = false;
   displayPhoto: string = "/assets/no-photo.png";
   
-  constructor(private productService: ProductService,
-    private route: ActivatedRoute) { }
+  constructor(
+    private productService: ProductService,
+    private route: ActivatedRoute,
+    private titleService: Title
+    ) { }
 
   ngOnInit() {
     var editId = this.route.snapshot.paramMap.get('id');
     if(!!editId) {
       this.productService.getSingle(editId).subscribe((i) => {
+        let finalPrice = i.price;
         this.infoURL = "https://api.whatsapp.com/send?phone=528115251019&text=Quisiera mas informacion de: " + i.title + ". " + encodeURI(window.location.href);
         this.displayPhoto = i.photos.length > 0 ? i.photos[0].url : "";
         i.description = i.description.replace(new RegExp('\n', 'g'), "<br />")
         if(i.percentageDiscount > 0) {
           this.hasDiscount = true;
           this.discountPercentagePrice = i.price - (i.price * (i.percentageDiscount / 100))
+          finalPrice = this.discountPercentagePrice;
         } else if (i.amountDiscount > 0) {
           this.hasDiscount = true;          
           this.discountAmountPrice = i.price - i.amountDiscount;
+          finalPrice = this.discountAmountPrice;
         }
         this.item = i
+        this.titleService.setTitle(i.title + " @ $" + finalPrice + " - En Alaska");
       });
     }
   }
